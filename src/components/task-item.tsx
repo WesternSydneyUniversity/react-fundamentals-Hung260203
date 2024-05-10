11eb71a55b36b1894b006d341ea84eb2fb3015fa
Hung260203 <136457206+Hung260203@users.noreply.github.com>
2024-05-10 17:43:58 +1000

    Assignment 2

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,8 +1,21 @@
 import type { Task } from "./task-list";
 
 import styles from "./task-item.module.css";
+import React from "react";
 
-export function TaskItem({ task }: { task: Task }) {
+export function TaskItem({ task, onDelete, onStateChange }: 
+  { task: Task, onDelete: (id: string) => void, 
+  onStateChange: (id: string, state: "PINNED" | "COMPLETED" | "ACTIVE") => void}){
+
+  const [taskState, changeState] = React.useState(task.state);
+
+  const titleClass = taskState === "COMPLETED" ? styles.titleCompleted: styles.title
+
+  const handleCheckboxChange = (e: { target: { checked: any; }; }) => {
+    changeState(taskState === "COMPLETED" ? "ACTIVE" : "COMPLETED");
+    onStateChange(task.id, taskState === "COMPLETED" ? "ACTIVE" : "COMPLETED");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.checkbox}>
@@ -11,15 +24,18 @@ export function TaskItem({ task }: { task: Task }) {
             type="checkbox"
             id={`task-${task.id}`}
             data-testid={`task-${task.id}`}
+            checked={taskState === "COMPLETED"}
+            onChange={handleCheckboxChange}
           />
           <label htmlFor={`task-${task.id}`}></label>
         </div>
       </div>
-      <span className={styles.title}>{task.title}</span>
+      <span className={titleClass}>{task.title}</span>
       <div className={styles.actions}>
         <button
           data-testid={`delete-${task.id}`}
           className={styles.deleteButton}
+          onClick={() => onDelete(task.id)}
         >
           Delete
         </button>
--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { MouseEventHandler, useEffect } from "react";
 import { TaskItem } from "./task-item";
 import styles from "./task-list.module.css";
 
@@ -10,15 +11,39 @@ export type Task = {
 };
 
 export function TaskList({ tasks }: { tasks: Task[] }) {
+  const [task, setTask] = React.useState(tasks);
+  const [title, setTitle] = React.useState("");
+
+  const deleteTask = (id: string) => {
+    const deleteTask = [...task];
+    deleteTask.splice(deleteTask.findIndex(task => task.id === id), 1);
+    setTask(deleteTask);
+  }
+
+  const changeState = (id: string, state: "PINNED" | "COMPLETED" | "ACTIVE") => {
+    const changeState = [...task];
+    changeState.find(task => task.id === id)!.state = state;
+    setTask(changeState);
+  }
+
+  const addTask = (event : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const id = (task.length + 1).toString();
+    const newTask: Task = {id: id, title: title, state: "ACTIVE"}; 
+    setTask([...task, newTask]);
+    setTitle("");
+  }
+
+  const activeTasks = task.filter(task => task.state === "ACTIVE").length;
+
   return (
     <>
       <div>
         <section className={styles.counter}>
-          <div className={styles.taskLabel}>0 tasks</div>
+          <div className={styles.taskLabel}>{activeTasks} tasks</div>
         </section>
         <section className={styles.section}>
-          {tasks.map((task) => (
-            <TaskItem key={task.id} task={task} />
+          {task.map((task) => (
+            <TaskItem key={task.id} task={task} onDelete={deleteTask} onStateChange={changeState}/>
           ))}
         </section>
       </div>
@@ -27,8 +52,10 @@ export function TaskList({ tasks }: { tasks: Task[] }) {
           type="text"
           placeholder="What needs to be done?"
           className={styles.taskInput}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <button className={styles.taskButton}>Add Task</button>
+        <button className={styles.taskButton} onClick={addTask}>Add Task</button>
       </section>
     </>
   );
